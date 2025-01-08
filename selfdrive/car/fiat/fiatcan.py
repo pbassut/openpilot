1bb74349ceb1c5551f10fe35ce479c4457938991
Patrick Bassut <patrickbassut@hotmail.com>
2025-01-08 19:55:16 -0300

    fibonnaci based

--- selfdrive/car/fiat/fiatcan.py
+++ selfdrive/car/fiat/fiatcan.py
@@ -1,4 +1,6 @@
+import time
 PT_BUS = 0
+
 DAS_BUS = 1
 
 def create_lkas_command(packer, frame, apply_steer, enabled):
@@ -10,14 +12,17 @@ def create_lkas_command(packer, frame, apply_steer, enabled):
   return packer.make_can_msg("LKAS_COMMAND", PT_BUS, values)
 
 def create_lkas_hud_command(packer, lat_active, eps_faulted):
+  seconds = int(time.time()) % 36
+  print("seconds: ", seconds)
+
   values = {
-    "SOMETHING_HANDS_ON_WHEEL_2": 0,
-    "SOMETHING_HANDS_ON_WHEEL": 0,
-    "HANDS_ON_WHEEL_WARNING": 1,
-    "LKAS_FAULTED_3": lat_active,
-    "LKAS_FAULTED_2": not lat_active,
+    "SOMETHING_HANDS_ON_WHEEL_2": seconds % 2 == 0,
+    "SOMETHING_HANDS_ON_WHEEL": seconds % 3 == 0,
+    "HANDS_ON_WHEEL_WARNING": seconds % 7 == 0,
+    "LKAS_FAULTED_3": seconds % 5 == 0,
+    "LKAS_FAULTED_2": seconds % 11 == 0,
     "LANE_HUD_INDICATOR": 6 if lat_active else 1,
-    "LKAS_STATUS": 0,
+    "LKAS_STATUS": seconds % 13 == 0,
     "LKAS_FAULT_TYPE": 7 if eps_faulted else 0,
   }
   return packer.make_can_msg("LKA_HUD_2", PT_BUS, values)
--- selfdrive/car/fiat/values.py
+++ selfdrive/car/fiat/values.py
@@ -33,8 +33,8 @@ class CAR(Platforms):
 class CarControllerParams:
   def __init__(self, CP):
     self.STEER_MAX = 1440
-    self.STEER_DELTA_UP = 6
-    self.STEER_DELTA_DOWN = 6
+    self.STEER_DELTA_UP = 3
+    self.STEER_DELTA_DOWN = 3
     self.STEER_ERROR_MAX = 80
 
     self.STEER_DRIVER_ALLOWANCE = 20
