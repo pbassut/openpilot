8e4c1d6a33d1dcf17eb707d7509243eddd5b6abe
Patrick Bassut <patrickbassut@hotmail.com>
2026-03-21 17:54:05 -0300

    byd

--- launch_env.sh
+++ launch_env.sh
@@ -5,6 +5,8 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
+export SKIP_FW_QUERY=True
+export FINGERPRINT=BYD_ATTO3
 
 # models get lower priority than ui
 # - ui is ~5ms
